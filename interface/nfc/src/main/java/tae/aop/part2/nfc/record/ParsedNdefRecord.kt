9817cc765b01744e959bf57f72aec73f71f4eba5
ktwktw109 <ktwktw109@gmail.com>
2023-11-20 18:51:23 +0900

    Integrated with NFC

---
+++ interface/nfc/src/main/java/tae/aop/part2/nfc/record/ParsedNdefRecord.kt
@@ -0,0 +1,18 @@
+package tae.aop.part2.nfc.record
+
+import android.app.Activity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+interface ParsedNdefRecord {
+    /**
+     * Returns a view to display this record.
+     */
+    fun getView(
+        activity: Activity,
+        inflater: LayoutInflater,
+        parent: ViewGroup,
+        offset: Int
+    ): View
+}
